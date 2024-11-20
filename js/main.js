6c0e3a89b72dfcdcb43eae18931b3dceaa13e2ec
Emanuel Victor Brebal <manu.brebal@gmail.com>
2024-11-20 19:27:57 -0300

    Second Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const navigation = document.querySelector(".primary-navigation");
+const navigationHeight = navigation.offsetHeight;
+
+document.documentElement.style.setProperty(
+    "--scroll-padding",
+    navigationHeight + "px"
+);
