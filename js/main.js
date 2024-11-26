7ab0a5c0b76ab67edba650f27a35da80b4de8dba
Emanuel <emanuel.brebal@seplag.al.gov.br>
2024-11-26 17:00:11 -0300

    site estruturado

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ document.documentElement.style.setProperty(
     "--scroll-padding",
     navigationHeight + "px"
 );
+
+const text = "Olá! Seja bem-vindo ao meu site!";
+let index = 0;
+
+function typeEffect() {
+    const typingDiv = document.getElementById('typingEffect');
+    if (index < text.length) {
+        typingDiv.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 100);
+    }
+}
+
+typeEffect();
+
